a586d924d69d2a11a9b573fa9fa3d7db9a86def9
yuvnn <yubin11890@gmail.com>
2026-07-30 13:26:07 +0900

    search 기능 업데이트

--- src/com/ssafy/trip/model/dao/TripDaoImpl.java
+++ src/com/ssafy/trip/model/dao/TripDaoImpl.java
@@ -54,7 +54,10 @@ public class TripDaoImpl implements TripDao {
 		// complete code #03
 		// List<TripDto> tripInfo 로부터 num 에 해당하는 TripDto 정보를 검색하여 return 하도록 코드를 작성하세요.
 		// 해당하는 num이 없을 경우 null을 리턴하세요.
-
+		for (TripDto trip : tripInfo) {
+			if (trip.getNum() == num) return trip;
+		}
+		
 		return null;
 	}
 
